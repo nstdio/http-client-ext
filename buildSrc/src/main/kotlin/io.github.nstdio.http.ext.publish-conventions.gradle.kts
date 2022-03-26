3d6d30e8eec75448976ea319e5714750ae906bde
Edgar Asatryan <nstdio@gmail.com>
2022-03-27 00:24:06 +0400

    fix(build): Remove sourcesJar and javadocJar tasks from publishing.

--- buildSrc/src/main/kotlin/io.github.nstdio.http.ext.publish-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io.github.nstdio.http.ext.publish-conventions.gradle.kts
@@ -25,8 +25,6 @@ publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
-            artifact(tasks["sourcesJar"])
-            artifact(tasks["javadocJar"])
 
             pom {
                 name.set("JDK Http Client Extensions")
