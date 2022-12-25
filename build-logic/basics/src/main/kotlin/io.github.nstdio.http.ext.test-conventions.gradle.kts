2422fa7aea2beff191ab2d56bffefb07beea7fee
Edgar Asatryan <nstdio@gmail.com>
2022-12-25 22:00:26 +0400

    Update equalsverifier (3.11.1 -> 3.12.3)

--- build-logic/basics/src/main/kotlin/io.github.nstdio.http.ext.test-conventions.gradle.kts
+++ build-logic/basics/src/main/kotlin/io.github.nstdio.http.ext.test-conventions.gradle.kts
@@ -71,7 +71,7 @@ val jacksonVersion = "2.14.0-rc2"
 val brotli4JVersion = "1.8.0"
 val brotliOrgVersion = "0.1.2"
 val gsonVersion = "2.10"
-val equalsverifierVersion = "3.11.1"
+val equalsverifierVersion = "3.12.3"
 val coroutinesVersion = "1.6.4"
 
 val jsonLibs = mapOf(
