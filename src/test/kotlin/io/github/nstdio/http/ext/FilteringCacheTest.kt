f06f216bb17a9ba99cb352a02072028dd6de61c6
Edgar Asatryan <nstdio@gmail.com>
2023-01-07 20:43:13 +0400

    Add tests for FilteringCache.

--- src/test/kotlin/io/github/nstdio/http/ext/FilteringCacheTest.kt
+++ src/test/kotlin/io/github/nstdio/http/ext/FilteringCacheTest.kt
@@ -15,21 +15,84 @@
  */
 package io.github.nstdio.http.ext
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.mock
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
 import org.mockito.Mockito.verify
+import org.mockito.Mockito.verifyNoInteractions
+import org.mockito.junit.jupiter.MockitoExtension
+import java.net.http.HttpRequest
+import java.time.Clock
 
+@ExtendWith(MockitoExtension::class)
 internal class FilteringCacheTest {
+  @Mock
+  private lateinit var mockDelegate: Cache
+
+  @Mock
+  private lateinit var mockRequest: HttpRequest
+
+  @Mock
+  private lateinit var mockEntry: Cache.CacheEntry
+  private lateinit var metadata: CacheEntryMetadata
+
+  @BeforeEach
+  fun setup() {
+    metadata = CacheEntryMetadata.of(0, 0, Helpers.responseInfo(mapOf()), mockRequest, Clock.systemUTC())
+  }
+
   @Test
   fun `Should call delegate`() {
     //given
-    val mock = mock(Cache::class.java)
-    val cache = FilteringCache(mock, { true }, { true })
+    val cache = FilteringCache(mockDelegate, { true }, { true })
 
     //when
+    cache.put(mockRequest, mockEntry)
+    cache.evict(mockRequest)
+    cache.evictAll(mockRequest)
+    cache.evictAll()
+    cache.get(mockRequest)
+    cache.stats()
+    cache.writer<String>(metadata)
     cache.close()
 
     //then
-    verify(mock).close()
+    verify(mockDelegate).put(mockRequest, mockEntry)
+    verify(mockDelegate).evict(mockRequest)
+    verify(mockDelegate).evictAll(mockRequest)
+    verify(mockDelegate).evictAll()
+    verify(mockDelegate).get(mockRequest)
+    verify(mockDelegate).stats()
+    verify(mockDelegate).writer<String>(metadata)
+    verify(mockDelegate).close()
+  }
+
+  @Test
+  fun `Should not call delegate when request does not match`() {
+    //given
+    val cache = FilteringCache(mockDelegate, { false }, { true })
+
+    //when
+    cache.put(mockRequest, mockEntry)
+    cache.evict(mockRequest)
+    cache.evictAll(mockRequest)
+    cache.get(mockRequest)
+    cache.writer<String>(metadata)
+
+    //then
+    verifyNoInteractions(mockDelegate)
+  }
+
+  @Test
+  fun `Should not call delegate when response does not match`() {
+    //given
+    val cache = FilteringCache(mockDelegate, { true }, { false })
+
+    //when
+    cache.writer<String>(metadata)
+
+    //then
+    verifyNoInteractions(mockDelegate)
   }
 }
