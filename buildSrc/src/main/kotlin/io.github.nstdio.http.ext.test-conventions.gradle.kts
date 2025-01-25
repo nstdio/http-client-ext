f03173c03d496d76b279725921762811917c63b4
Edgar Asatryan <nstdio@gmail.com>
2025-01-26 02:36:36 +0400

    Bump org.assertj:assertj-core from 3.24.2 to 3.27.3

--- buildSrc/src/main/kotlin/io.github.nstdio.http.ext.test-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io.github.nstdio.http.ext.test-conventions.gradle.kts
@@ -62,7 +62,7 @@ mapOf(
 }
 
 val junitVersion = "5.10.1"
-val assertJVersion = "3.24.2"
+val assertJVersion = "3.27.3"
 val kotestAssertionsVersion = "5.8.0"
 val mockitoVersion = "5.7.0"
 val jsonPathAssertVersion = "2.7.0"
